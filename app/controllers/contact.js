48388ce959b04e11e7425b1f1b31210d229a3370
Brad Erickson <BradE@Magenic.com>
2017-01-18 13:54:28 -0600

    first commit

---
+++ app/controllers/contact.js
@@ -0,0 +1,19 @@
+import Ember from 'ember';
+
+export default Ember.Controller.extend({
+    message: '',
+    emailAddress: '',
+    isDisabled: Ember.computed('emailAddress', 'message', function() {
+        if(this.emailAddress.includes('@') && this.message.length > 5){
+            return false
+        } else {
+            return true;
+        }
+    }),
+    actions: {
+        sendMessage() {
+            alert(`Sending message to ${this.get('emailAddress')}`);
+            this.set('responseMessage', `Email sent successfully to ${this.get('emailAddress')}`);
+        }
+    },
+});
